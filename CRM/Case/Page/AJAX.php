cb05b0cae4ae8ab6faebe4e8d6909de2806f397f
Muhammad Shahrukh <>
2024-10-24 16:26:49 +0500

    COMCL-885: Apply CIVI-SA-2024-05
    Core commits
    
    https://github.com/civicrm/civicrm-core/commit/cddfcf35a0c76f71e12fcb22a2dc15139287efbb

--- CRM/Case/Page/AJAX.php
+++ CRM/Case/Page/AJAX.php
@@ -24,6 +24,7 @@ class CRM_Case_Page_AJAX {
    * @throws \CRM_Core_Exception
    */
   public static function processCaseTags() {
+    CRM_Core_Page_AJAX::validateAjaxRequestMethod();
 
     $caseId = CRM_Utils_Type::escape($_POST['case_id'], 'Positive');
     $tags = CRM_Utils_Type::escape($_POST['tag'], 'String');
@@ -84,6 +85,7 @@ class CRM_Case_Page_AJAX {
    * @throws \CiviCRM_API3_Exception
    */
   public function caseDetails() {
+    CRM_Core_Page_AJAX::validateAjaxRequestMethod();
     $caseId = CRM_Utils_Type::escape($_GET['caseId'], 'Positive');
 
     $case = civicrm_api3('Case', 'getsingle', [
@@ -112,6 +114,7 @@ class CRM_Case_Page_AJAX {
    * @throws \CRM_Core_Exception
    */
   public function addClient() {
+    CRM_Core_Page_AJAX::validateAjaxRequestMethod();
     $caseId = CRM_Utils_Type::escape($_POST['caseID'], 'Positive');
     $contactId = CRM_Utils_Type::escape($_POST['contactID'], 'Positive');
 
@@ -155,6 +158,7 @@ class CRM_Case_Page_AJAX {
    * Delete relationships specific to case and relationship type.
    */
   public static function deleteCaseRoles() {
+    CRM_Core_Page_AJAX::validateAjaxRequestMethod();
     $caseId = CRM_Utils_Type::escape($_POST['case_id'], 'Positive');
     $cid = CRM_Utils_Type::escape($_POST['cid'], 'Positive');
     $relType = CRM_Utils_Request::retrieve('rel_type', 'String', CRM_Core_DAO::$_nullObject, TRUE);
@@ -170,6 +174,7 @@ class CRM_Case_Page_AJAX {
   }
 
   public static function getCases() {
+    CRM_Core_Page_AJAX::validateAjaxRequestMethod();
     $requiredParameters = [
       'type' => 'String',
     ];
