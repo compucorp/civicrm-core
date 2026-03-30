377c34e41cb345c9305b5125a550055b78bec8d4
shahrukh <shahrukh@compuco.io>
2026-03-30 18:08:39 +0500

    COMCL-1421: Apply security patch

---
+++ Civi/Api4/Action/File/FileSaveTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ +--------------------------------------------------------------------+
+ | Copyright CiviCRM LLC. All rights reserved.                        |
+ |                                                                    |
+ | This work is published under the GNU AGPLv3 license with some      |
+ | permitted exceptions and without any warranty. For full license    |
+ | and copyright information, see https://civicrm.org/licensing       |
+ +--------------------------------------------------------------------+
+ */
+
+namespace Civi\Api4\Action\File;
+
+trait FileSaveTrait {
+
+  /**
+   * @inheritDoc
+   */
+  protected function write(array $items) {
+    foreach ($items as &$file) {
+      // In update mode, the uri cannot be changed.
+      if (!empty($file['id']) && isset($file['uri'])) {
+        $existingUri = \CRM_Core_DAO_File::getDbVal('uri', $file['id']);
+        if ($existingUri !== $file['uri']) {
+          throw new \CRM_Core_Exception("Uri of existing file cannot be changed.");
+        }
+        unset($file['uri']);
+      }
+      // In create mode, uri cannot be set.
+      if (isset($file['uri'])) {
+        throw new \CRM_Core_Exception("Setting file URI is not permitted. Use file_name instead.");
+      }
+    }
+    return \CRM_Core_BAO_File::writeRecords($items);
+  }
+
+}
